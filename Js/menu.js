3acf31af6cfcd83f4713f15aeb93e721de3e1be0
Gabriel <ggabrielcunha06@gmail.com>
2025-06-01 16:05:44 -0300

    teste

---
+++ Js/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".ul");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+});
